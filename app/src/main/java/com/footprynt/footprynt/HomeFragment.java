e75f3558544ba449f204353706651b184e58bafd
Karan Bheda <bhedakaran1@gmail.com>
2017-03-13 18:24:48 +0530

    Fixed Home and Profile page

--- app/src/main/java/com/footprynt/footprynt/HomeFragment.java
+++ app/src/main/java/com/footprynt/footprynt/HomeFragment.java
@@ -1,21 +1,21 @@
 package com.footprynt.footprynt;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 import com.uguratar.countingtextview.countingTextView;
 
 public class HomeFragment extends Fragment {
-    //private TextView miles;
-    private countingTextView miles,offer_claimed,exclusive;
+    private countingTextView miles,offer_claimed,exclusive,general;
     private ViewGroup rootView;
+    private RelativeLayout rr_general, rr_exclusive;
 
     public static HomeFragment newInstance(){
         HomeFragment homefragment = new HomeFragment();
@@ -26,13 +26,28 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = (ViewGroup) inflater.inflate(R.layout.home_layout, container, false);
+        //initializing all elements
         miles = (countingTextView) rootView.findViewById(R.id.tv_miles);
         offer_claimed = (countingTextView) rootView.findViewById(R.id.tv_offers_claimed);
         exclusive = (countingTextView) rootView.findViewById(R.id.tv_exclusive);
+        general = (countingTextView) rootView.findViewById(R.id.tv_general);
+        rr_general = (RelativeLayout) rootView.findViewById(R.id.rr_general);
+        rr_exclusive = (RelativeLayout) rootView.findViewById(R.id.rr_exclusive);
 
+
+        //adding animations to all the numerical values
         miles.animateText(0,10000000);
         offer_claimed.animateText(0,7);
         exclusive.animateText(0,4);
+        general.animateText(0,12);
+
+        GradientDrawable drawable1 = (GradientDrawable) rr_exclusive.getBackground();
+        drawable1.setStroke(3, getResources().getColor(R.color.orange));
+
+        GradientDrawable drawable2 = (GradientDrawable) rr_general.getBackground();
+        drawable2.setStroke(3, getResources().getColor(R.color.purple));
+
+
         return rootView;
     }
 
--- app/src/main/java/com/footprynt/footprynt/ProfileFragment.java
+++ app/src/main/java/com/footprynt/footprynt/ProfileFragment.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,6 +16,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class ProfileFragment extends Fragment {
     private ViewGroup rootView;
     private CircleImageView twitter, fb,edit_dp;
+    private TextView status_fb, status_twitter;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
@@ -22,11 +26,15 @@ public class ProfileFragment extends Fragment {
         fb = (CircleImageView) rootView.findViewById(R.id.check_fb);
         twitter = (CircleImageView) rootView.findViewById(R.id.check_twitter);
         edit_dp = (CircleImageView) rootView.findViewById(R.id.btn_change_dp);
+        status_fb = (TextView) rootView.findViewById(R.id.status_fb);
+        status_twitter = (TextView) rootView.findViewById(R.id.status_twitter);
 
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 fb.setImageDrawable(getResources().getDrawable(R.drawable.ic_active_fb));
+                status_fb.setText("Connected");
+                status_fb.setTextColor(getResources().getColor(R.color.green));
             }
         });
 
@@ -34,6 +42,8 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 twitter.setImageDrawable(getResources().getDrawable(R.drawable.ic_active_twitter));
+                status_twitter.setText("Connected");
+                status_twitter.setTextColor(getResources().getColor(R.color.green));
             }
         });
 
